298ae4af440a248908601711cd7c0758f20c9573
asr2010 <arpitshekharraj@gmail.com>
2023-09-24 23:10:33 -0400

    REST API content negotiation, Internaltionalization, Versioning

--- src/main/java/com/arpit/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/arpit/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.arpit.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	// /hello-world
 	//@RequestMapping(method = RequestMethod.GET, path="/hello-world")
 	@GetMapping(path="/hello-world")
@@ -22,6 +31,12 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	@GetMapping(path="/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale =  LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+		//return "Hello World V2";
 	}
 
 }
