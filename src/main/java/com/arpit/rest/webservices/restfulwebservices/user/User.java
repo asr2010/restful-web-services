10d51c0c529ec0d714e3633e9733b41b2b5604e9
asr2010 <arpitshekharraj@gmail.com>
2023-10-05 08:58:24 -0400

    Static filtering on fileds

--- src/main/java/com/arpit/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/arpit/rest/webservices/restfulwebservices/user/User.java
@@ -2,6 +2,8 @@ package com.arpit.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,7 @@ public class User {
 	
 	private int id;
 	@Size(min = 2, message="Name should have atleast 2 characters")
+	@JsonProperty("user_name")
 	private String name;
 	@Past(message = "Birth Date should be in the past")
 	private LocalDate birthDate;
