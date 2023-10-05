983cde174fa49848df6e5df7d0c851943aa4b99f
asr2010 <arpitshekharraj@gmail.com>
2023-10-05 08:30:15 -0400

    HATEOAS to enhance api by returning link to all users with specific user request

--- src/main/java/com/arpit/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/arpit/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.arpit.rest.webservices.restfulwebservices.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,12 +36,18 @@ public class UserResource {
 	
 	//Retrive details of a specific user
 	@GetMapping(path="/users/{id}")
-	public User retrieveUser(@PathVariable int id){
+	public EntityModel<User> retrieveUser(@PathVariable int id){
 		User user = service.findOne(id);
 		
 		if(user == null)
 			throw new UserNotFoundException("id: "+id);
-		return user;
+		
+		EntityModel<User> entityModel = EntityModel.of(user);
+		
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		
+		entityModel.add(link.withRel("all-users"));
+		return entityModel;
 	}
 	
 	@DeleteMapping(path="/users/{id}")
